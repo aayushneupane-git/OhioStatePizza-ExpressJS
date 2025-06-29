9890e03dbe338d9b39a572ebdeddd044866c9c93
Aayush Neupane <aayushneupane55@gmail.com>
2025-06-29 16:41:32 -0400

    Normal

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
-// const Stripe = require("stripe");
-// const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 exports.createPaymentIntent = async (req, res) => {
-  // const { amount } = req.body; // amount in dollars
-  // try {
-  //   const paymentIntent = await stripe.paymentIntents.create({
-  //     amount: Math.round(amount * 100), // convert to cents
-  //     currency: "usd",
-  //     payment_method_types: ["card"],
-  //   });
-  //   res.status(200).json({ clientSecret: paymentIntent.client_secret });
-  // } catch (err) {
-  //   console.error("Stripe Error:", err.message);
-  //   res.status(500).json({ error: err.message });
-  // }
+  const { amount } = req.body; // amount in dollars
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100), // convert to cents
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    console.error("Stripe Error:", err.message);
+    res.status(500).json({ error: err.message });
+  }
 };
