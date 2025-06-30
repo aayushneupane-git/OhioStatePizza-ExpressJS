064a6d45dc137d842054f53051b1ac8905cc9990
Aayush Neupane <aayushneupane55@gmail.com>
2025-06-30 11:49:22 -0400

    sada

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -9,7 +9,6 @@ const authorize = require('../middleware/roleMiddleware');
 // 🟢 Login route
 router.post('/login', employeeController.loginEmployee);
 router.get('/profile',authenticate, employeeController.getEmployeeProfile);
-
 // Manager creates cook
 router.post('/create-cook', authenticate, authorize('manager'), employeeController.createCook);
 
