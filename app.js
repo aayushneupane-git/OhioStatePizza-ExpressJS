2fc159e8114a403260ccbcd554c310b644b4ddfe
Shishir Paudel <sishirpaudel7@gmail.com>
2025-07-02 23:46:45 -0400

    push

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 // 🟢 Body parsing middleware (optional, if you’re using JSON)
 app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // 🟢 API routes
 app.use("/api/specials", specialRoutes);
